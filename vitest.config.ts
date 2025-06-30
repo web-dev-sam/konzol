66d5c896042cad43ba02222d240a4d2faf90492c
Samuel Braun <sam@webry.com>
2025-06-30 19:25:31 +0200

    Add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['test/**/*.test.ts'],
+    coverage: {
+      exclude: [
+        'main.ts',
+        '*.config.ts',
+        'assets/**',
+        'dist/**',
+        'src/vite.ts',
+        'src/index.ts'
+      ]
+    }
   },
 })
