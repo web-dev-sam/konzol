8019ff8e19beadb8b98b714e27ef9f49bb31b552
Samuel Braun <sam@webry.com>
2025-07-06 14:22:55 +0200

    Update configuration to ensure no floating promises

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,21 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   type: 'lib',
+  typescript: true,
   rules: {
     'no-console': ['error', {
       allow: ['log', 'info', 'warn', 'error'],
     }],
   },
   ignores: ['./src/parser', './assets', './dist'],
+}, {
+  files: ['**/*.ts'],
+  languageOptions: {
+    parserOptions: {
+      project: './tsconfig.json',
+    },
+  },
+  rules: {
+    'ts/no-floating-promises': ['error'],
+  },
 })
