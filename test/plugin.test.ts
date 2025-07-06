513858c8a086c3095327afb0c98611d14d4734bc
Samuel Braun <sam@webry.com>
2025-07-06 14:25:28 +0200

    uncomment currently not used code

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,6 +1,6 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { transform } from '../src/core/transform'
-import { expectToBeCalledWith, run } from './utils/utils'
+import { expectError, run } from './utils/utils'
 
 vi.stubGlobal('console', {
   warn: vi.fn(),
@@ -14,33 +14,34 @@ describe('invalid plugin usage', () => {
     vi.clearAllMocks()
   })
 
-  it('should handle missing plugin parameters', async () => {
-    const error = vi.mocked(console.error)
-    // eslint-disable-next-line no-eval
-    await eval(
-      transform('', 'main.ts', undefined!,
-      ) as any,
-    )
-    expectToBeCalledWith(error, 'error', 'Options are not provided')
-  })
+  // it('should handle missing plugin parameters', async () => {
+  //   const error = vi.mocked(console.error)
+  //   // eslint-disable-next-line no-eval
+  //   await eval(
+  //     transform('', 'main.ts', undefined!,
+  //     ) as any,
+  //   )
+  //   expectToBeCalledWith(error, 'error', 'Options are not provided')
+  // })
 
-  it('should handle missing option fields', async () => {
-    const error = vi.mocked(console.error)
-    // eslint-disable-next-line no-eval
-    await eval(
-      transform('', 'main.ts', {} as any,
-      ) as any,
-    )
+  // it('should handle missing option fields', async () => {
+  //   const error = vi.mocked(console.error)
+  //   // eslint-disable-next-line no-eval
+  //   await eval(
+  //     transform('', 'main.ts', {} as any,
+  //     ) as any,
+  //   )
 
-    expectToBeCalledWith(error, 'error', 'Options are not provided')
-  })
+  //   expectToBeCalledWith(error, 'error', 'Options are not provided')
+  // })
 
   it('should ignore non-supported files', async () => {
     // eslint-disable-next-line no-eval
     const result = await eval(
-      transform('', 'main.json', {} as any,
-      ) as any,
-    )?.error
+
+      transform('', 'main.json', {}) as any,
+
+    )?.error as any
     expect(result).toBe(undefined)
   })
 })
@@ -82,5 +83,43 @@ describe('loaders', () => {
       <script></script>
     `, { id: 'App.vue' })
     expect(log).toHaveBeenCalledTimes(0)
+  })
+})
+
+describe('config', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should handle custom macro name', async () => {
+    const log = vi.mocked(console.log)
+    await run(`yoo('YOOO???')`, { id: 'main.jsx' }, {
+      macroName: 'yoo',
+    })
+    expect(log).toHaveBeenCalledWith('YOOO???')
+  })
+
+  it('should warn about invalid custom macro name', async () => {
+    await expectError('!yoo(\'YOOO???\')', 'Invalid macro name', { id: 'main.ts' }, {
+      macroName: '!yoo',
+    })
+  })
+
+  it('should warn about invalid custom macro name 2', async () => {
+    await expectError('yoo!!(\'YOOO???\')', 'Invalid macro name', { id: 'main.tsx' }, {
+      macroName: 'yoo!!',
+    })
+  })
+
+  it('should accept valid macro name', async () => {
+    await expectError('yoo!(\'YOOO???\')', 'Macro names ending with "!" are only supported in TypeScript files', { id: 'main.js' }, {
+      macroName: 'yoo!',
+    })
+  })
+
+  it('should warn about ts syntax macro in js files', async () => {
+    await expectError('yoo!(\'YOOO???\')', 'Macro names ending with "!" are only supported in TypeScript files', { id: 'main.jsx' }, {
+      macroName: 'yoo!',
+    })
   })
 })
